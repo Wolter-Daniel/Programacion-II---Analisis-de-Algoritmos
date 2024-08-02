34cd169f1d5b13fb240c3907c9d399a767f77965
Daniel Wolter <[147331762+Wolter-Daniel@users.noreply.github.com]>
2024-08-02 17:19:55 -0300

    Correccion de clase y prueba de creacion de nodo

--- Analisis de Algoritmos/linked_list.py
+++ Analisis de Algoritmos/linked_list.py
@@ -3,10 +3,18 @@
     UCASAL. Lic.Ciencia de Datos.
     Alumno: Daniel Andres Wolter.
 '''
-class Nodo (Int):
+class Nodo (object):
     def __init__(self, dato = None, prox = None ):
 
         #Constructor
-        
+
         self.dato = dato
         self.prox = prox
+
+v3 = Nodo (234)
+v2 = Nodo (875, v3)
+v1 = Nodo (999, v2)
+
+print(v1.dato)
+print(v2.dato)
+print(v3.dato)
