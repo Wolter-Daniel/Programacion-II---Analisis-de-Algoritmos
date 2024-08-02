be0bd29d7cf387b36b4c87d46c583661c9b30312
Daniel Wolter <[147331762+Wolter-Daniel@users.noreply.github.com]>
2024-08-02 20:08:29 -0300

    Mejorar documentacion del modulo

--- Analisis de Algoritmos/linked_list.py
+++ Analisis de Algoritmos/linked_list.py
@@ -18,17 +18,18 @@ class Nodo (object):
             nodo = nodo.prox
 
 class Linked_list (object):
+    '''Craer un objeto de tipo lista enlazada'''
     def __init__ (self):
         self.prim = None
 
-    # Método para agregar elementos en el frente de la linked list
 
     def add_at_front (self, data):
+        '''Método para agregar elementos en el frente de la linked list'''
         self.prim = Nodo(dato = data, prox = self.prim)
 
-    # Metodo para agregar elementos al final de la lista
 
     def add_at_end (self, data):
+        '''Metodo para agregar elementos al final de la lista'''
         if self.prim == None:
             self.add_at_front(data)
             return
@@ -39,9 +40,9 @@ class Linked_list (object):
             else:
                 nodo.prox = Nodo(data) 
 
-    #Metodo para buscar elemento
 
     def search_data (self, data):
+        '''Metodo para buscar elemento'''
         nodo = self.prim
         while nodo.prox:
             if nodo.dato == data:
@@ -51,9 +52,9 @@ class Linked_list (object):
         else:
             print("Dato no existente") 
 
-    #Metodo para eliminar un dato
 
     def delete_nodo (self, data):
+        '''Metodo para eliminar un dato'''
         nodo = self.prim
         prev = None
         while nodo.prox:
@@ -69,19 +70,31 @@ class Linked_list (object):
         else:
             print("Dato no existente")
 
-    # Método para verificar si la estructura de datos esta vacia
 
     def is_empty (self):
+        '''Método para verificar si la estructura de datos esta vacia'''
         empety = "True" if self.prim == None else "False"
         print(empety)
     
-    # Metodo para mostrar lista.
-    
+
     def print_list (self):
+        '''Metodo para mostrar lista.'''
         nodo = self.prim
         while nodo != None :
             print("Dato:", nodo.dato, "prox:", nodo.prox)
             nodo = nodo.prox
+    
+
+    def length (self):
+        '''Metodo para obtener cantidad de elementos'''
+        count = 0
+        nodo = self.prim
+        while nodo.prox:
+            count = count + 1
+            nodo = nodo.prox
+        else:
+            count = count + 1
+        return count
 
 v3 = Nodo ("Manzanas")
 v2 = Nodo ("Peras", v3)
@@ -106,9 +119,9 @@ linked_list.add_at_end("!")
 linked_list.add_at_end("!")
 linked_list.add_at_front("#")
 linked_list.print_list()
-
+print(linked_list.length())
 #linked_list.is_empty()
+#linked_list.delete_nodo("Hola")
+#linked_list.print_list()
 
-linked_list.delete_nodo("Hola")
-
-linked_list.print_list()
+Linked_list.
