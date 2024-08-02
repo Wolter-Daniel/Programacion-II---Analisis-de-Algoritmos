989855f8af50525dbb077a33de43d1ab8b62904d
Daniel Wolter <[147331762+Wolter-Daniel@users.noreply.github.com]>
2024-08-02 18:49:25 -0300

    Crear la clase linked list, crear metodo de insercion al inicio y verificacion de lista vacia

--- Analisis de Algoritmos/linked_list.py
+++ Analisis de Algoritmos/linked_list.py
@@ -7,7 +7,7 @@ class _Nodo (object):
 
     #Constructor
 
-    def __init__(self, dato = None, prox = None ):
+    def __init__ (self, dato = None, prox = None ):
         self.dato = dato
         self.prox = prox
 
@@ -15,6 +15,22 @@ class _Nodo (object):
         while nodo:
             print(nodo.dato)
             nodo = nodo.prox
+
+class _linked_list (Object):
+    def __init__ (self):
+        self.prim = None
+
+    # Método para agregar elementos en el frente de la linked list
+
+    def add_at_front(self, dato):
+        self.prim = _Nodo(data = dato, prox = self.prim)
+
+    # Método para verificar si la estructura de datos esta vacia
+
+    def is_empety (self):
+        return False if self.prim == None else True
+    
+    
 
 v3 = _Nodo ("Manzanas")
 v2 = _Nodo ("Peras", v3)
