8e4c22d35f5fa506b53a9144e4523678f2a79863
Daniel Wolter <[147331762+Wolter-Daniel@users.noreply.github.com]>
2024-08-04 11:41:15 -0300

    Se agrego metodo para imprimir la lista en reversa, yse agrego un puntero para el ultimo elemento

--- Analisis de Algoritmos/double_linked_list.py
+++ Analisis de Algoritmos/double_linked_list.py
@@ -8,18 +8,29 @@ class Double_linked_list (object):
     '''Craer un objeto'''
     def __init__ (self):
         self.prim = None
+        self.last = None
 
     def add_at_front (self, data):
+        '''Insertar elemento al comienzo de la lista'''
         new_nodo = Nodo(dato = data, prev = None, prox = self.prim)
         if self.prim != None:
             self.prim.prev = new_nodo
-        self.prim = new_nodo
+        else:
+            self.prim = new_nodo
+        if self.last == None:
+            self.last = self.prim
           
 
     def print_list (self):
             '''Metodo para mostrar lista.'''
             nodo = self.prim
             while nodo != None :
-                print("Prev:",nodo.prev, "Nodo:", nodo.dato, "prox:", nodo.prox)
+                print("Dato:", nodo.dato)
                 nodo = nodo.prox
 
+    def print_revert_list (self):
+            '''Metodo para mostrar la lista invertida.'''
+            nodo = self.last
+            while nodo != None :
+                print("Dato:", nodo.dato)
+                nodo = nodo.prev
