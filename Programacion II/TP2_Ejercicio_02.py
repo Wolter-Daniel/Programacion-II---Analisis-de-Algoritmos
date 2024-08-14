1e67dd7ede6ce87c4f10db3b5a9ec28896b16661
Daniel Wolter <[147331762+Wolter-Daniel@users.noreply.github.com]>
2024-08-14 19:33:58 -0300

    Ejercicio 02 TP 2

---
+++ Programacion II/TP2_Ejercicio_02.py
@@ -0,0 +1,21 @@
+'''
+    Trabajo Practico 2
+    UCASAL. Lic.Ciencia de Datos.
+    Alumno: Daniel Andres Wolter.
+'''
+
+#Dados dos números enteros cualquiera almacenados en variables, almacenar en 
+#una nueva variable la resta de ellos e imprimir por pantalla su resultado.
+
+class TP2_Ejercio_02 ():
+    def main():
+        print("Restaremos 2 numeros reales!!!")
+        num1 = 5
+        num2 = 4
+
+        result = (num1 - num2)
+
+        print("La resta de ", num1, "-", num2, "=", result)
+
+if __name__ == "__main__":
+    TP2_Ejercio_02.main()
