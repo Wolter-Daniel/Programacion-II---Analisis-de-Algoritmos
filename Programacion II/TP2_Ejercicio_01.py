6ec7136db4e0623503d60b8b9365742c52a9a0d9
Daniel Wolter <[147331762+Wolter-Daniel@users.noreply.github.com]>
2024-08-14 19:28:58 -0300

    Ejercicio 1 TP-2

---
+++ Programacion II/TP2_Ejercicio_01.py
@@ -0,0 +1,22 @@
+'''
+    Trabajo Practico 2
+    UCASAL. Lic.Ciencia de Datos.
+    Alumno: Daniel Andres Wolter.
+'''
+
+#Dados dos números enteros cualquiera almacenados en variables, 
+#almacenar en una nueva variable la suma de ellos,
+#imprimir por pantalla su resultado.
+
+class TP2_Ejercicio_01 ():
+    def main():
+        print("Sumaremos 2 numeros reales!!!")
+        num1 = 5
+        num2 = 4
+
+        result = (num1 + num2)
+
+        print("La suma de ", num1, "+", num2, "=", result)
+
+if __name__ == "__main__":
+    TP2_Ejercicio_01.main()
