3439b39b4eacabefbea32801ec8fa3c2ac411ad8
Daniel Wolter <[147331762+Wolter-Daniel@users.noreply.github.com]>
2024-08-14 20:03:21 -0300

    TP2 Ejercicio 05

---
+++ Programacion II/TP2_Ejercicio_05.py
@@ -0,0 +1,19 @@
+'''
+    Trabajo Practico 2
+    UCASAL. Lic.Ciencia de Datos.
+    Alumno: Daniel Andres Wolter.
+'''
+
+#Ídem ejercicio 4 pero una vez ingresados nombres y apellidos, almacenarlos en 
+#una sola variable y mostrar el mensaje de bienvenida. 
+
+
+class TP2_Ejercio_ ():
+    def main():
+        nombre = input("Ingrese su nombre:")
+        apellido = input("Ingrese su apellido:")
+        nombre_completo = nombre + " " + apellido
+        print("¡¡¡Bienvenido", nombre_completo, "!!!")
+
+if __name__ == "__main__":
+    TP2_Ejercio_.main()
