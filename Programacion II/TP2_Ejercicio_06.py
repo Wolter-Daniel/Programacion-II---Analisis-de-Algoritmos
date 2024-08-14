f4c97f923312a29a627940b24de1daa1ceace592
Daniel Wolter <[147331762+Wolter-Daniel@users.noreply.github.com]>
2024-08-14 20:23:35 -0300

    TP2 Ejercicio 06

---
+++ Programacion II/TP2_Ejercicio_06.py
@@ -0,0 +1,20 @@
+'''
+    Trabajo Practico 2
+    UCASAL. Lic.Ciencia de Datos.
+    Alumno: Daniel Andres Wolter.
+'''
+
+#Solicitar al usuario el ingreso de dos números, calcular y mostrar el promedio de 
+#ambos valores.
+
+class TP2_Ejercio_ ():
+    def main():
+        print("Calcularemos el promedio entre dos numeros")
+        num1 = int(input("Ingrese un numero: "))
+        num2 = int(input("Ingrese el segundo numero: "))
+
+        print("El promedio es de: ", ((num1 + num2) / 2) )
+
+
+if __name__ == "__main__":
+    TP2_Ejercio_.main()
